5bffb56af82ecb7090abcb4d2257369f49574fe1
xl.meng <xl.meng@mobiletek.cn>
2018-11-30 17:47:21 +0800

    Add C sharp test project.

---
+++ HelloCSharp/HelloCSharp/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace HelloCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "字体切换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 18F);
+            this.textBox1.Location = new System.Drawing.Point(809, 469);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 316);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "",
+            "4",
+            "",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(178, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(38, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "3";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(31, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(971, 39);
+            this.panel1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 11F);
+            this.button2.Location = new System.Drawing.Point(3, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(37, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "高亮";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(807, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "临时粘贴板";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.richTextBox1.Location = new System.Drawing.Point(31, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(758, 739);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(88, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "h3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ItemHeight = 12;
+            this.comboBox2.Items.AddRange(new object[] {
+            "红",
+            "",
+            "橙",
+            "",
+            "黄",
+            "",
+            "绿",
+            "",
+            "蓝",
+            "",
+            "靛",
+            "",
+            "紫"});
+            this.comboBox2.Location = new System.Drawing.Point(378, 8);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(95, 20);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "红";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(778, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "删除星号";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox2.Font = new System.Drawing.Font("宋体", 15F);
+            this.richTextBox2.Location = new System.Drawing.Point(809, 75);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(193, 372);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(880, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "插入竖线";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 795);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Markdown edit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ HelloCSharp/HelloCSharp/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace HelloCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fontsize;
+            fontsize = "<font color=#ff0000 size=5 face=\"黑体\">" + richTextBox1.SelectedText + "</font>";
+            //richTextBox1.AppendText("你已经被选中");
+            richTextBox1.Text = "";
+            richTextBox1.AppendText(fontsize);
+           // MessageBox.Show("你已经被选中");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveNextItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorPositionItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "3":
+                    setFormat("<font size=3 >", "</font>");
+                    break;
+                case "4":
+                    setFormat("<font size=4 >", "</font>");
+                    break;
+                case "5":
+                    setFormat("<font size=5 >", "</font>");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string boldstr = "**";
+            string tempstr;
+            tempstr = richTextBox1.SelectedText;
+            richTextBox1.SelectionColor = Color.FromName("red");
+            richTextBox1.SelectedText = boldstr;
+            richTextBox1.SelectionColor = Color.FromName("yellow");
+            richTextBox1.SelectedText = tempstr;
+            richTextBox1.SelectionColor = Color.FromName("red");
+            richTextBox1.SelectedText = boldstr;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string highstr = "==";
+            string tempstr ;
+            tempstr = richTextBox1.SelectedText;
+            richTextBox1.SelectionColor = Color.FromName("red");
+            richTextBox1.SelectedText = highstr;
+            richTextBox1.SelectionColor = Color.FromName("yellow");
+            //richTextBox1.SelectionColor = Color.FromArgb(222,33,44);
+            richTextBox1.SelectedText = tempstr;
+            richTextBox1.SelectionColor = Color.FromName("red");
+            richTextBox1.SelectedText = highstr;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int firstCharPosition = richTextBox1.GetFirstCharIndexOfCurrentLine();
+            int lineNumber = richTextBox1.GetLineFromCharIndex(firstCharPosition);
+            int lastCharPosition = richTextBox1.GetFirstCharIndexFromLine(lineNumber + 1);
+
+            richTextBox1.Select(firstCharPosition, lastCharPosition - firstCharPosition);
+            richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.SelectedText = "//" + richTextBox1.SelectedText.ToString();
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {         
+            switch (comboBox2.Text)
+            {
+                case "红":
+                    setFormat("<font color=#ff0000 >**", "**</font>");
+                    break;
+                case "橙":
+                    setFormat("<font color=#ffa500 >**", "**</font>");
+                    break;
+                case "黄":
+                    setFormat("<font color=#ffff00 >**", "**</font>");
+                    break;
+                case "绿":
+                    setFormat("<font color=#00ff00 >**", "**</font>");
+                    break;
+                case "蓝":
+                    setFormat("<font color=#0000ff >**", "**</font>");
+                    break;
+                case "靛":
+                    setFormat("<font color=#ff0000 >**", "**</font>");
+                    break;
+                case "紫":
+                    setFormat("<font color=#800080 >**", "**</font>");
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void setFormat(string str1, string str2) {
+            string tmp = richTextBox1.SelectedText;
+            richTextBox1.SelectionColor = Color.FromName("red");
+            richTextBox1.SelectedText = str1;
+            richTextBox1.SelectionColor = Color.FromName("yellow");
+            richTextBox1.SelectedText = tmp;
+            richTextBox1.SelectionColor = Color.FromName("red");
+            richTextBox1.SelectedText = str2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string tmp = richTextBox2.SelectedText;
+            //tmp.Replace("*", "");
+            tmp =tmp.Replace("*", string.Empty);
+            //tmp.Trim('*');
+            richTextBox2.SelectedText = tmp;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string tmp = richTextBox2.SelectedText;
+            //tmp.Replace("*", "");
+           // tmp = tmp.Replace(" ", "|");
+            string pattern = @"[\f\t\r\v]";
+            tmp = Regex.Replace(tmp, pattern, "|");
+            //tmp.Trim('*');
+            richTextBox2.SelectedText = tmp;
+        }
+    }
+}
